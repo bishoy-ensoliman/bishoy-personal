72e478485c4ea488b2c6f748c41658ccbdb065c6
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2023-09-24 14:05:00 +0200

    style fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import NextThemeProvider from '../providers/NextThemeProvider';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Bishoy Soliman',
