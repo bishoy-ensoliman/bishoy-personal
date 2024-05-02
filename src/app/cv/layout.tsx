600e68ceab6d7591e6bbc700ec7808571ad0b10d
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2024-05-02 16:11:49 +0200

    change the CV page

--- src/app/cv/layout.tsx
+++ src/app/cv/layout.tsx
@@ -7,7 +7,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
                 <div className="mx-auto w-full max-w-7xl lg:px-8">
                     <div className="relative px-4 sm:px-8 lg:px-12">
                         <div className="mx-auto max-w-2xl lg:max-w-5xl">
-                            <div className="float-right">
+                            <div className="float-left">
                                 <DownloadCV />
                             </div>
                             <div className="max-w-2xl">
