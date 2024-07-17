262f74480fe59d41d607dcdf86712f9aa67c78e8
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2024-07-17 22:47:32 +0200

    restart deployments

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ git pull
 docker build . -t localhost:32000/personal-site:latest
 docker push localhost:32000/personal-site:latest
 docker image prune -f
+# TODO use helm charts
 microk8s kubectl apply -f ./kubernetes/namespace.yaml
 microk8s kubectl apply -f ./kubernetes/personalsite-deployment.yaml
 microk8s kubectl apply -f ./kubernetes/personalsite-service.yaml
 microk8s kubectl apply -f ./kubernetes/ingress.yaml
+microk8s kubectl rollout restart deployments/personalsite -n bishoy-personalsite
