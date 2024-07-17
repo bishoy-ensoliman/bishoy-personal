b2cc32fad00546b1fceb27cc758625e338b747b5
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2024-07-17 21:34:10 +0200

    fix start script

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ git pull
 docker build . -t localhost:32000/personal-site:latest
 docker push localhost:32000/personal-site:latest
 docker image prune -f
-microk8s kubectl apply -f namespace.yaml
-microk8s kubectl apply -f personal-site-deployment.yaml
-microk8s kubectl apply -f personal-site-service.yaml
-microk8s kubectl apply -f personal-site-ingress.yaml
+microk8s kubectl apply -f ./kubernetes/namespace.yaml
+microk8s kubectl apply -f ./kubernetes/personal-site-deployment.yaml
+microk8s kubectl apply -f ./kubernetes/personal-site-service.yaml
+microk8s kubectl apply -f ./kubernetes/personal-site-ingress.yaml
