571e03236d0890f277b2bd76fe10aea1b2eead9f
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2024-07-17 21:36:32 +0200

    fix typos

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ docker build . -t localhost:32000/personal-site:latest
 docker push localhost:32000/personal-site:latest
 docker image prune -f
 microk8s kubectl apply -f ./kubernetes/namespace.yaml
-microk8s kubectl apply -f ./kubernetes/personal-site-deployment.yaml
-microk8s kubectl apply -f ./kubernetes/personal-site-service.yaml
-microk8s kubectl apply -f ./kubernetes/personal-site-ingress.yaml
+microk8s kubectl apply -f ./kubernetes/personalsite-deployment.yaml
+microk8s kubectl apply -f ./kubernetes/personalsite-service.yaml
+microk8s kubectl apply -f ./kubernetes/ingress.yaml
