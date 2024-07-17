be74519b194edee6449eb34e981c5e73acd5dae7
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2024-07-17 21:22:21 +0200

    add k8s deployment

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd ~/projects/bishoy-personal
 git pull
-docker compose build
+docker build . -t localhost:32000/personal-site:latest
+docker push localhost:32000/personal-site:latest
 docker image prune -f
-docker compose up -d
+microk8s kubectl apply -f namespace.yaml
+microk8s kubectl apply -f personal-site-deployment.yaml
+microk8s kubectl apply -f personal-site-service.yaml
+microk8s kubectl apply -f personal-site-ingress.yaml
