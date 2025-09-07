9e716937be13efa739787fdac2e9e97f76ddc16e
Bishoy Soliman <bishoy.ensoliman@gmail.com>
2025-09-07 03:35:09 +0200

    more style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
           </div>
         </div>
         <div className="mt-16 sm:mt-20 flex justify-center">
-          <div className="flex gap-5 overflow-y-scroll py-4 sm:gap-8">
+          <div className="flex gap-5 overflow-x-scroll overflow-y-hidden py-4 sm:gap-8 px-4">
             <div
               className="relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl -rotate-2">
               <Image alt="coding" className="absolute inset-0 h-full w-full object-cover" src={codePic} />
